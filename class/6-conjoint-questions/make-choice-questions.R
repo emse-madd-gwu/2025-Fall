c84a50c23b47fc85a83be66e2da920698578a70a
emse-p4a-gwu <john.helveston@gmail.com>
2025-08-10 14:33:11 -0400

    init

---
+++ class/6-conjoint-questions/make-choice-questions.R
@@ -0,0 +1,53 @@
+# Make conjoint surveys using the cbcTools package
+
+# Install packages
+# install.packages("fastDummies")
+# install.packages("cbcTools")
+
+# Load libraries
+library(cbcTools)
+library(fastDummies)
+library(here)
+
+# Define profiles with attributes and levels
+profiles <- cbc_profiles(
+    price       = c(15, 20, 25), # Price ($1,000)
+    fuelEconomy = c(20, 25, 30),   # Fuel economy (mpg)
+    accelTime   = c(6, 7, 8),      # 0-60 mph acceleration time (s)
+    powertrain  = c("Gasoline", "Electric")
+)
+
+# Make a full-factorial design of experiment 
+design <- cbc_design(
+    profiles = profiles,
+    n_resp   = 1000, # Number of respondents
+    n_alts   = 3,    # Number of alternatives per question
+    n_q      = 8     # Number of questions per respondent
+)
+
+head(design) # preview
+
+# Make a labeled design with "powertrain" as the label
+design_labeled <- cbc_design(
+    profiles = profiles,
+    n_resp   = 1000, # Number of respondents
+    n_alts   = 3,    # Number of alternatives per question
+    n_q      = 8,    # Number of questions per respondent
+    label = 'powertrain'
+)
+
+head(design_labeled) # preview
+
+# Make a survey with a "no choice" option
+design_nochoice <- cbc_design(
+    profiles = profiles,
+    n_resp   = 1000, # Number of respondents
+    n_alts   = 3,    # Number of alternatives per question
+    n_q      = 8,    # Number of questions per respondent
+    no_choice = TRUE
+)
+
+head(design_nochoice) # preview
+
+# Save design
+write_csv(design, here('choice_questions.csv'))
